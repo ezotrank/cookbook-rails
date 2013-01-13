a94d9c2eef8d399b5201f4bfc8de6c80dc1c1323
Maxim Kremenev <ezotrank@gmail.com>
2013-01-13 15:58:11 +0300

    create user, install rubies

--- libraries/chef_rails_user_helpers.rb
+++ libraries/chef_rails_user_helpers.rb
@@ -2,6 +2,19 @@ class Chef
   module RAILS
     module UserHelpers
 
+      # Install all versions of ruby
+      def install_rubies
+        rubies = []
+        node.rails_apps.each do |app|
+          rubies << data_bag_item('rails_apps', app)['environments'].map {|e| e['ruby_version']}
+        end
+        rubies = rubies.flatten.uniq
+        node.set['rvm']['rubies'] = rubies
+        node.set['rvm']['branch']  = "stable"
+        include_recipe "rvm::system"
+      end
+
+      # Just create user
       def create_user(params)
         rails_account params['login'] do
           ssh_keys params['ssh_keys']
@@ -14,6 +27,22 @@ class Chef
             members params['login']
             append true
           end
+        end
+      end
+
+      # Define default user ruby
+      def default_user_ruby(user, ruby_version)
+        find_string = "export RVM_DEFAULT=#{ruby_version}"
+        bashrc = File.join("/home", user, ".bashrc")
+
+        if %x[grep "#{find_string}" #{bashrc}].empty?
+          tmp_file = "/tmp/#{user}_#{ruby_version}"
+          template tmp_file do
+            source "default_user_ruby.erb"
+            variables({ :ruby_version => ruby_version })
+          end
+          %x[cat #{tmp_file}| tee -a #{bashrc}]
+          FileUtils.rm_rf tmp_file
         end
       end
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,11 +2,14 @@ class Chef::Recipe
   include Chef::RAILS::UserHelpers
 end
 
+# Install all rubies versions from all rails apps
+install_rubies
+
 node.rails_apps.each do |app|
   puts "App Name: #{app}"
-
   rails_app = data_bag_item('rails_apps', app)
   rails_app['environments'].each do |env|
     create_user(env['user'])
+    default_user_ruby(env['user']['login'], env['ruby_version'])
   end
 end
