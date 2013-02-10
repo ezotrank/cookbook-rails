a6777ffb6281515136a81cb83330493cb99e2422
Maxim Kremenev <ezotrank@gmail.com>
2013-02-10 16:36:46 +0300

    change install libv8 package on ubuntu system

--- recipes/install_v8.rb
+++ recipes/install_v8.rb
@@ -2,7 +2,7 @@ packages = case node['platform_family']
            when "rhel", "fedora", "suse"
              ['v8', 'v8-devel']
            when "debian"
-             ['libv8-3.8.9.20', 'libv8-dev']
+             ['libv8-dev']
            end
 
 packages.each do |v8_pack|
